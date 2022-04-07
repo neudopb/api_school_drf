c10fb1ba59e690f3b6ed36db07f488b947a43362
neudopb <neudinho66@gmail.com>
2022-04-07 16:29:14 -0300

    feat: Custom Admin

--- cursos/admin.py
+++ cursos/admin.py
@@ -1,9 +1,45 @@
 from django.contrib import admin
-from cursos.models import (
+from .models import (
     Course,
     Evaluation,
 )
 
 
-admin.site.register(Course)
-admin.site.register(Evaluation)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "url",
+        "created",
+        "updated",
+        "is_active",
+    )
+    search_fields = (
+        "title",
+        "url",
+    )
+    list_filter = (
+        "is_active",
+    )
+
+@admin.register(Evaluation)
+class EvaluationAdmin(admin.ModelAdmin):
+    list_display = (
+        "course",
+        "name",
+        "email",
+        "note",
+        "created",
+        "updated",
+        "is_active",
+    )
+    search_fields = (
+        "course__title",
+        "name",
+        "email",
+    )
+    list_filter = (
+        "course__title",
+        "email",
+        "is_active",
+    )
