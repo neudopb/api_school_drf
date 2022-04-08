1efe0b4a8d3c347178d901e35e265ef12d36829d
neudopb <neudinho66@gmail.com>
2022-04-08 08:28:31 -0300

    feat: Creation of HTTP GET endpoints

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import (
+    Course,
+    Evaluation,
+)
+from .serializers import (
+    CourseSerializer,
+    EvaluationSerializer,
+)
 
-# Create your views here.
+
+class CourseAPIView(APIView):
+    """
+    API Courses
+    """
+    def get(self, request):
+        courses = Course.objects.all()
+        serializer = CourseSerializer(courses, many=True)
+        return Response(serializer.data)
+
+    
+class EvaluationAPIView(APIView):
+    """
+    API Evaluation
+    """
+    def get(self, request):
+        evaluations = Evaluation.objects.all()
+        serializer = EvaluationSerializer(evaluations, many=True)
+        return Response(serializer.data)
